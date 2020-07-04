efc5d83879dcac67bd67e05944aeb1c34074c115
Dogukan Aktas <aktasdogukann@gmail.com>
2020-07-05 00:15:34 +0300

    Add country details

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import styles from "../css/CountryDetail.module.css";
 
-const ItemDetail = () =>  {
-
-    const {name} = useParams();
+const CountryDetail = ({ countries }) => {
+  const details = countries.map((val) => val);
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className={styles.container}>
+      <div className={styles.subcontainer}>
+        <h1 className={styles.name}>
+          {countries.map((val) => val.name.toUpperCase())}
+        </h1>
+        <img
+          className={styles.flag}
+          src={countries.map((val) => val.flag)}
+          alt=""
+        />
+
+        <h2 className={styles.region}>{countries.map((val) => val.region)}</h2>
+        <h2 className={styles.subregion}>
+          {countries.map((val) => val.subregion)}
+        </h2>
+        <span>Borders: </span>
+        <h4>{countries.map((val) => val.borders.map((tek) => tek + " | "))}</h4>
+      </div>
     </div>
   );
-}
+};
 
-export default ItemDetail
+export default CountryDetail;
