ce60d083f60e235c11ecbe4aa78b403ed51d716f
Dogukan Aktas <aktasdogukann@gmail.com>
2020-07-05 04:23:52 +0300

    Add country details

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,28 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { useParams } from "react-router-dom";
 import styles from "../css/CountryDetail.module.css";
+import {BsArrowLeft} from 'react-icons/bs'
 
-const CountryDetail = ({ countries }) => {
-  const details = countries.map((val) => val);
+const CountryDetail = (props) => {
+  const { countries, history } = props;
+  console.log(countries);
   return (
     <div className={styles.container}>
       <div className={styles.subcontainer}>
-        <h1 className={styles.name}>
-          {countries.map((val) => val.name.toUpperCase())}
-        </h1>
-        <img
-          className={styles.flag}
-          src={countries.map((val) => val.flag)}
-          alt=""
-        />
+        
+        <button className={styles.backButton} onClick={() => history.goBack()}>
+        <BsArrowLeft className={styles.arrow}/> <span className={styles.backButtonText}>Back</span>
+        </button>
 
-        <h2 className={styles.region}>{countries.map((val) => val.region)}</h2>
-        <h2 className={styles.subregion}>
-          {countries.map((val) => val.subregion)}
-        </h2>
-        <span>Borders: </span>
-        <h4>{countries.map((val) => val.borders.map((tek) => tek + " | "))}</h4>
+        <div className={styles.leftSub}>
+          <h1 className={styles.name}>
+            {countries.map((val) => val.name.toUpperCase())}
+          </h1>
+          <img
+            className={styles.flag}
+            src={countries.map((val) => val.flag)}
+            alt=""
+          />
+        </div>
+
+        <div className={styles.rightSub}>
+          <div>
+            <span>region:</span>
+            <h5 className={styles.region}>
+              {countries.map((val) => val.region)}
+            </h5>
+          </div>
+          <div>
+            <span>subregion:</span>
+            <h5 className={styles.subregion}>
+              {countries.map((val) => val.subregion)}
+            </h5>
+          </div>
+          <div>
+            <span>population:</span>
+            <h5 className={styles.population}>
+              {countries.map(val => val.population ? val.population.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "No")}
+            </h5>
+          </div>
+          <div>
+            <span>area:</span>
+            <h5 className={styles.area}>{countries.map((val) => val.area ? (
+                    <span>
+                      {val.area.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} km<sup>2</sup>
+                    </span>
+                  ) : (
+                    "0"
+                  ))}</h5>
+          </div>
+          <div>
+            <span>currencies:</span>
+            <h5 className={styles.currencies}>
+              {`${countries.map((val) => val.currencies.map((cur) => cur.name))} - ${countries.map((val) => val.currencies.map((cur) => cur.symbol))}`}
+            </h5>
+          </div>
+          <div>
+            <span>borders:</span>
+            <h5 className={styles.borders}>
+              {countries.map((val) => val.borders.map((bor) => bor + "  "))}
+            </h5>
+          </div>
+          <div>
+            <span>languages:</span>
+            <h5>{countries.map(val => val.languages.map(lang=> lang.name+' '))}</h5>
+          </div>
+        </div>
       </div>
     </div>
   );
