8ba35952e65d8c5a26ca074d6fd956c0be62ee30
Dogukan Aktas <aktasdogukann@gmail.com>
2020-07-07 01:40:23 +0300

    Add style names and change icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Filter from "./Filter";
-import styles from "../css/Main.module.css";
 import { GrCurrency } from "react-icons/gr";
 import { FiStar } from "react-icons/fi";
 import { FaGlobe } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { FaLanguage } from "react-icons/fa";
 import { GoGlobe } from "react-icons/go";
+import styles from "../css/Main.module.css";
+
 
 const Main = ({ search, country, setSearch, setCountry }) => {
   return (
@@ -20,11 +21,7 @@ const Main = ({ search, country, setSearch, setCountry }) => {
       />
 
       <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(5, auto)",
-          gridGap: "1em",
-        }}
+      className={styles.gridContainer}
       >
         {country
           .filter(
@@ -33,41 +30,57 @@ const Main = ({ search, country, setSearch, setCountry }) => {
           )
           .map((val, id) => (
             <div key={id}>
-              <div>
-                <Link to={`/country/${val.alpha3Code}`}>{val.name}</Link>
-                <div>{`(${val.nativeName})`}</div>
-                <div>
-                  <FiStar /> {val.capital || "No Capital"}
+              <div className={styles.gridBox}>
+
+                <div className={styles.imageContainer}>
+                  <img className={styles.image} src={val.flag} />
                 </div>
-                <img className={styles.image} src={val.flag} />
-                <div>
-                  <FaGlobe />{" "}
+
+                <div className={styles.name}>
+                <Link className={styles.nameText} to={`/country/${val.alpha3Code}`}>{val.name.toUpperCase()}</Link>
+                </div>
+
+                <div className={styles.nativeName}>{`( ${val.nativeName} )`}</div>
+
+                <div className={styles.capital}>
+                <FiStar className={styles.capitalIcon} /> {val.capital || "No Capital"}
+                </div>
+
+                
+
+                <div className={styles.region}>
+                  <FaGlobe className={styles.regionIcon} />{" "}
                   {val.region
                     ? val.region + " - " + val.subregion
                     : "Unspecified"}
                 </div>
-                <div>
-                  <FaLanguage /> {val.languages.map((lang) => lang.name + " ")}
+
+                <div className={styles.languages}>
+                  <FaLanguage className={styles.languagesIcon}/> {val.languages.map((lang) => lang.name + " ")}
                 </div>
-                <div>
-                  <GrCurrency />{" "}
+
+                <div className={styles.currencies}>
+                  <GrCurrency className={styles.currenciesIcon} />{" "}
                   {val.currencies.map(
                     (cur) => cur.code === "(none)" ? "" : cur.code === null ? "" : cur.code + " "
                   )}
                 </div>
-                <div>
-                  <BsFillPeopleFill /> {val.population ? val.population.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "0"}
+
+                <div className={styles.population}>
+                  <BsFillPeopleFill className={styles.populationIcon} /> {val.population ? val.population.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') : "0"}
                 </div>
-                <div>
-                  <GoGlobe />{" "}
+
+                <div className={styles.area}>
+                  <GoGlobe className={styles.areaIcon} />{" "}
                   {val.area ? (
-                    <span>
+                    <span className={styles.areaSpan}>
                       {val.area.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} km<sup>2</sup>
                     </span>
                   ) : (
                     "0"
                   )}
                 </div>
+
               </div>
             </div>
           ))}
