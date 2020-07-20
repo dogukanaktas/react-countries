a7f335e20c3a3926351b05c3df4c8506ec325239
Dogukan Aktas <aktasdogukann@gmail.com>
2020-07-20 15:29:45 +0300

    Add error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import styles from "../css/Error.module.css";
+import {BsArrowLeft} from "react-icons/bs"
 
-const Error = () => {
-    return (
-        <div>
-            <h1>404 error</h1>
-        </div>
-    )
-}
+const Error = ({history}) => {
+  return (
+    <div className={styles.errorContainer}>
+      <h1 className={styles.error}>404 Error Page</h1>
+      <h2 className={styles.sorry}>Sorry, this page doesn't exist</h2>
+      <h3 className={styles.goBackError} onClick={() => history.goBack()}>
+      <BsArrowLeft className={styles.goBackArrow} />   <span className={styles.goBackSpan}>Go Back</span>
+      </h3>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
