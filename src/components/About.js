2365c340c8abadb123b84341bde2b42c40e6f70e
Dogukan Aktas <aktasdogukann@gmail.com>
2020-08-28 23:02:13 +0300

    Add classnames

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { GoMarkGithub } from 'react-icons/go'
+import styles from '../css/About.module.css'
 
 
 const About = () =>  {
     return (
-        <div>
-            <GoMarkGithub />
-            <span> github.com/dogukanaktas</span>
+        <div className={styles.about}>
+            <GoMarkGithub className={styles.logo} />
+            <span className={styles.github}> github.com/dogukanaktas</span>
         </div>
     )
 }
